ae7c37e30ba4fa9b02177087837db6db0aa72631
Sumit kumar <119700621+xonx4l@users.noreply.github.com>
2023-07-03 19:57:36 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,6 +17,25 @@ std::std::vector<std::string>  read_pokemon_names(){
 }
 
 
-void catch_pokemon(int& pokeballs, int&money, std:std::vector<std::string>& caught_pokemon) {
+void catch_pokemon(int& pokeballs, int&money, std::vector<std::string>& caught_pokemon) {
+    std::vector<std::string> pokemons = read_pokemon_names();
+    std::string pokemon = pokemons[rand()% pokemons.size()];
+    std::cout<<"a wild "<< pokemon << "appears!"<<std::endl;
+    std::cout<<"enter 'c' to catch or 'R'  to run:";
+    char action;
+    std::cin>>action;
+    if(action == "c" || action == 'c'){
+        if (pokeballs >0){
+            pokeballs--;
+            if(static_cast<double>(rand()) / RAND_MAX < 0.5 )) {
+              std::cout << "You caught the" << pokemon << "!"<< std::endl;
+              caught_pokemon.push_back(pokemon)
+             }else {
+              std::cout << pokemon << "escaped!"<< std:: endl;
+            }
+        }else {
+         std::cout <<"you don't have any pokeballs" << std::endl;
+        }
+    }
     
 }
